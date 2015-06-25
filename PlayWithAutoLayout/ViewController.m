1a0a514e5ec9800e6f83b723e2954afd83c568ce
katrin-polyakova <katrin21@ukr.net>
2015-06-25 14:36:27 +0300

    UIBuilder

---
+++ PlayWithAutoLayout/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  PlayWithAutoLayout
+//
+//  Created by Kate Polyakova on 6/20/15.
+//  Copyright (c) 2015 Kate Polyakova. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(NSUInteger)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskAll;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
